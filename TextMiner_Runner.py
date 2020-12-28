30fb4fe525aacc5dc8fcbbeed93d79f2f044498b
WULH <wulhan@outlook.com>
2020-12-28 09:03:33 +0800

    update the Runner

--- TextMiner_Runner.py
+++ TextMiner_Runner.py
@@ -2,9 +2,10 @@ import os
 
 def run_app():
     # Environment configuration
+    success = False
     try:
-        py_version = os.system('python --version')
-        print(py_version)
+        os.system('python --version')
+        os.system('pip --version')
         os.system('pip install streamlit==0.72.0')
         os.system('pip install pandas==0.25.3')
         os.system('pip install numpy==1.19.3')
@@ -13,16 +14,19 @@ def run_app():
         os.system('pip install matplotlib==3.1.1')
         os.system('pip install wordcloud==1.8.1')
     except Exception as e:
-        print('Error:', e)
+        print('Exception : ', e)
         return
-    finally:
-        print('\n------->Environment configuration is successful!')
-    # run app
-    try:
-        os.system('streamlit run Text_Miner.py')
-    except Exception as e:
-        print('Error:', e)
-        return
+    else:
+        print('\n-------> Environment configuration is successful!')
+        success = True
+    
+    if success:
+        # run app
+        try:
+            os.system('streamlit run Text_Miner.py')
+        except Exception as e:
+            print('Error:', e)
+            return
 
 if __name__ == "__main__":
     run_app()
